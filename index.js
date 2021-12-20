825afb00ca0e3563cc07c0df65b7db9580f9294f
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-20 10:59:03 -0500

    commit tree 54fc305ed8223944aac150c767193f04a2c414a0

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^-\w\s'-]/gi,'')
+  }
+  static titleize(string) {
+    const wordArr = string.split(' ')
+    const resultArr = []
+    resultArr.push(this.capitalize(wordArr[0]))
+    const exeptanceList = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let i=1; i< wordArr.length; i++) {
+      if (exeptanceList.includes(wordArr[i])){
+        resultArr.push(wordArr[i])
+      } else {
+        resultArr.push(this.capitalize(wordArr[i]))
+      }
+    }
+    return resultArr.join(" ")
+  }
 }
